1a9ab5cb076cd79133bea8046573698f492db9a8
felipe-14 <andres.grisales.1405@gmail.com>
2021-12-12 09:31:51 -0600

    Incidencias, actualizacion BD

--- API/Startup.cs
+++ API/Startup.cs
@@ -20,7 +20,7 @@ namespace API
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration;   
         }
 
         public IConfiguration Configuration { get; }
